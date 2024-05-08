b4efb5f127a19fc19c0562e528664e84959f8b6d
Binh 🌱 <binhrobles@gmail.com>
2024-05-08 18:49:43 -0400

    move script out

---
+++ instrument.js
@@ -0,0 +1,8 @@
+const map = new L.Map('map');
+map.setView([40.693, -73.985], 14, false);
+
+new L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© Map tiles <a href="https://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  maxNativeZoom: 20
+}).addTo(map);
