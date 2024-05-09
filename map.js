b34e3fd41dc2525566349645e1010d6d529deaf3
Binh 🌱 <binhrobles@gmail.com>
2024-05-09 14:53:58 -0400

    3d maps

--- map.js
+++ map.js
@@ -7,8 +7,7 @@ new L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 18,
   maxNativeZoom: 20
 }).addTo(map);
-new OSMBuildings(map).load();
-
+new OSMBuildings(map).load('https://{s}-data.onegeo.co/maps/tiles/{z}/{x}/{y}.json?token={TOKEN}', { fixedZoom: 15 });
 
 // ------ control initialization ------ //
 const control = L.control({ position: 'topleft' });
